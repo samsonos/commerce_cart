7c864af795dd24474f9233b89e7dd5938e579105
Kotenko <nick.w2r@gmail.com>
2015-01-05 15:51:18 +0200

    add controllers

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kotenko on 04.01.2015.
+ */
+
+s('.samsonos-commerce-cart-add-btn').pageInit(function(obj){
+    obj.ajaxSubmit(function(responce){
+
+    })
+})
--- src/Cart.php
+++ src/Cart.php
@@ -18,4 +18,44 @@ use samson\core\CompressableService;
 class Cart extends CompressableService
 {
     public $id = 'cart';
+
+	public $commerceCore;
+	public $popupView = 'popup.php';
+	public $cartIndexView = 'index.php';
+	public $itemView = 'item.php';
+	public $renderer;
+
+	public function init(array $params=array())
+	{
+		parent::init( $params );
+		if (isset($this->renderer)) {
+			$this->renderer = & m($this->renderer);
+		} else {
+			$this->renderer = & $this;
+		}
+		Event::fire('commerce.init.module.commerce.core',array(& $this));
+	}
+
+	public function __HANDLER()
+	{
+		$orders = $this->commerceCore->ordersList();
+		$rows = '';
+		foreach ($orders as $order) {
+			foreach ($order->items as $item) {
+				$rows .= $this->renderer->view($this->itemView)->item($item)->product($item->Product)->output();
+			}
+		}
+		$this->renderer->view($this->cartIndexView)->rows($rows);
+	}
+
+	public function __async_add($productId)
+	{
+		$response = array('status'=>0);
+		$count = isset($_POST['count'])?$_POST['count'] : 1;
+		if ($this->commerceCore->addOrderItem($productId, $count) !== false) {
+			$response['status'] = 1;
+			$response['html'] = $this->renderer->view($this->popupView)->output();
+		}
+		return $response;
+	}
 }
